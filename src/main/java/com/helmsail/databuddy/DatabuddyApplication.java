b8da7d3c1020753951600d91fee885cd38c670c7
hejye <1959365428@qq.com>
2026-09-17 10:06:45 +0800

    chore: 基于 Spring Initializr 初始化单模块工程

---
+++ src/main/java/com/helmsail/databuddy/DatabuddyApplication.java
@@ -0,0 +1,13 @@
+package com.helmsail.databuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helmsail/databuddy/DatabuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helmsail.databuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
